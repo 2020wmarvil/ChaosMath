a69bcda9d9ef06c9a93c3355f057ad7097ebdcbe
Wyatt Marvil <2020wmarvil@gmail.com>
2021-11-25 14:36:26 -0500

    Basic vec4 with constructors and copy+assignment+swap

--- ChaosMath/ChaosMath/cm.h
+++ ChaosMath/ChaosMath/cm.h
@@ -1,7 +1,33 @@
 #pragma once
 
-#include <iostream>
+// supported types:
+// vec2f, vec3f, vec4f
+// vec2i, vec3i, vec4i
 
 namespace cm {
-	void Pog() { std::cout << "pog" << std::endl; }
+
+	template <typename T>
+	struct vec4 {
+		T x, y, z, w;
+
+		vec4() : vec4(0, 0, 0, 0) {}
+		vec4(T all) : vec4(all, all, all, all) {}
+		vec4(T _x, T _y, T _z, T _w) : x(_x), y(_y), z(_z), w(_w) { }
+		vec4(const vec4& v) { x = v.x; y = v.y; z = v.z; w = v.w; }
+		~vec4() {}
+
+		void swap(vec4& other) {
+			std::swap(x, other.x);
+			std::swap(y, other.y);
+			std::swap(z, other.z);
+			std::swap(w, other.w);
+		}
+
+		vec4& operator=(vec4 rhs) {
+			swap(rhs);
+			return *this;
+		}
+	};
+
+	typedef vec4<float> vec4f;
 }
--- ChaosMath/ChaosMath/driver.cpp
+++ ChaosMath/ChaosMath/driver.cpp
@@ -3,8 +3,26 @@
 #include "cm.h"
 
 int main() {
-	std::cout << "hi" << std::endl;
-	cm::Pog();
+	cm::vec4f v;
+	cm::vec4f v1(1);
+	cm::vec4f v2(1, 2, 3, 4);
+	cm::vec4f v3(v);
+
+
+	std::cout << v2.x << ", " << v2.y << ", " << v2.z << ", " << v2.w << std::endl;
+	std::cout << v3.x << ", " << v3.y << ", " << v3.z << ", " << v3.w << std::endl;
+
+	v2 = v3;
+
+	std::cout << v2.x << ", " << v2.y << ", " << v2.z << ", " << v2.w << std::endl;
+	std::cout << v3.x << ", " << v3.y << ", " << v3.z << ", " << v3.w << std::endl;
+
+	std::cout << v.x << ", " << v.y << ", " << v.z << ", " << v.w << std::endl;
+	std::cout << v1.x << ", " << v1.y << ", " << v1.z << ", " << v1.w << std::endl;
+
+	v.swap(v1);
+	std::cout << v.x << ", " << v.y << ", " << v.z << ", " << v.w << std::endl;
+	std::cout << v1.x << ", " << v1.y << ", " << v1.z << ", " << v1.w << std::endl;
 
 	return 0;
 }
