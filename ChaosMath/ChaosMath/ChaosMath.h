7e309e407c39bcecd84101ea643b8dd5b2456cdc
Wyatt Marvil <2020wmarvil@gmail.com>
2021-11-25 19:03:52 -0500

    Member functions

--- ChaosMath/ChaosMath/ChaosMath.h
+++ ChaosMath/ChaosMath/ChaosMath.h
@@ -1,32 +1,55 @@
 #pragma once
 
+#include <assert.h>
+
 // supported types:
 // vec2f, vec3f, vec4f
 // vec2i, vec3i, vec4i
 
-// magnitude, magnitudeSquared, normalized
-// dot, cross, 
+// static functions
+// lerp, pow, dot, cross
+// static members
 // one, zero, up, down, left, right, forward, back, ana, kata
 
+// convert from each type to each other type
+
 namespace cm {
+	template <typename T>
+	struct vec3 {
+		T x, y, z;
+
+		vec3() : vec3(0, 0, 0) {}
+		vec3(T all) : vec3(all, all, all) {}
+		vec3(T _x, T _y, T _z) : x(_x), y(_y), z(_z) { }
+		vec3(const vec3& v) { x = v.x; y = v.y; z = v.z; }
+		~vec3() {}
+	};
+
+	template<typename T>
+	inline std::ostream& operator<<(std::ostream &os, vec3<T> vec) { 
+		os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")"; 
+		return os; 
+	}
+
 	template <typename T>
 	struct vec4 {
 		T x, y, z, w;
 
-		vec4() : vec4(0, 0, 0, 0) {}
-		vec4(T all) : vec4(all, all, all, all) {}
+		vec4() : vec4<T>(0, 0, 0, 0) {}
+		vec4(T all) : vec4<T>(all, all, all, all) {}
 		vec4(T _x, T _y, T _z, T _w) : x(_x), y(_y), z(_z), w(_w) { }
-		vec4(const vec4& v) { x = v.x; y = v.y; z = v.z; w = v.w; }
+		vec4(const vec4<T>& v) { x = v.x; y = v.y; z = v.z; w = v.w; }
 		~vec4() {}
 
-		void swap(vec4& other) {
+		#pragma region MEMBER OVERLOADS
+		void swap(vec4<T>& other) {
 			std::swap(x, other.x);
 			std::swap(y, other.y);
 			std::swap(z, other.z);
 			std::swap(w, other.w);
 		}
 
-		vec4& operator=(vec4 rhs) {
+		vec4<T>& operator=(vec4<T> rhs) {
 			swap(rhs);
 			return *this;
 		}
@@ -34,11 +57,11 @@ namespace cm {
 		inline bool operator==(const vec4<T>& rhs) { return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w; }
 		inline bool operator!=(const vec4<T>& rhs) { return !operator==(rhs); }
 
-		vec4 operator-() const { 
-			return vec4(-x, -y, -z, -w);
+		vec4<T> operator-() const { 
+			return vec4<T>(-x, -y, -z, -w);
 		}
 
-		vec4& operator+=(const vec4& rhs) {
+		vec4<T>& operator+=(const vec4<T>& rhs) {
 			x += rhs.x;
 			y += rhs.y;
 			z += rhs.z;
@@ -46,7 +69,7 @@ namespace cm {
 			return *this;
 		}
 
-		vec4& operator-=(const vec4& rhs) {
+		vec4<T>& operator-=(const vec4<T>& rhs) {
 			x -= rhs.x;
 			y -= rhs.y;
 			z -= rhs.z;
@@ -54,7 +77,7 @@ namespace cm {
 			return *this;
 		}
 
-		vec4& operator*=(const T& rhs) {
+		vec4<T>& operator*=(const T& rhs) {
 			x *= rhs;
 			y *= rhs;
 			z *= rhs;
@@ -62,15 +85,45 @@ namespace cm {
 			return *this;
 		}
 
-		vec4& operator/=(const T& rhs) {
+		vec4<T>& operator/=(const T& rhs) {
 			x /= rhs;
 			y /= rhs;
 			z /= rhs;
 			w /= rhs;
 			return *this;
 		}
+		#pragma endregion
+
+		#pragma region MEMBER FUNCTIONS
+		inline float magnitude() const {
+			return std::sqrt(x*x + y*y + z*z + w*w);
+		}
+
+		inline float sqrMagnitude() const {
+			return x*x + y*y + z*z + w*w;
+		}
+
+		inline vec4<T> normalized() const {
+			float magnitude = this->magnitude();
+			assert(magnitude != 0.0f);
+			return vec4<T>(x, y, z, w) / magnitude;
+		}
+
+		inline float dot(const vec4<T>& v) const {
+			return x * v.x + y * v.y + z * v.z + w * v.w;
+		}
+
+		inline vec3<T> cross(const vec4<T>& v) const {
+			return vec3<T>(
+				y * v.z - z * v.y,
+				-(x * v.z - z * v.x),
+				x * v.y - y * v.x
+			);
+		}
+		#pragma endregion
 	};
 
+	#pragma region NON-MEMBER OVERLOADS
 	template <typename T>
 	inline vec4<T> operator+(vec4<T> lhs, const vec4<T>& rhs) {
 		lhs += rhs;
@@ -106,6 +159,7 @@ namespace cm {
 		os << "(" << vec.x << ", " << vec.y << ", " << vec.z << ", " << vec.w << ")"; 
 		return os; 
 	}
+	#pragma endregion
 
 	typedef vec4<float> vec4f;
 }
--- ChaosMath/ChaosMath/driver.cpp
+++ ChaosMath/ChaosMath/driver.cpp
@@ -3,7 +3,7 @@
 #include "ChaosMath.h"
 
 int main() {
-	cm::vec4f v;
+	cm::vec4f v0;
 	cm::vec4f v1(1);
 	cm::vec4f v2(1, 2, 3, 4);
 	cm::vec4f v3(v2);
